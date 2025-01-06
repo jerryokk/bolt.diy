c716e40eea976d2f7f0a582df40a4fc2a27b111e
jerryokk <126859008+jerryokk@users.noreply.github.com>
2025-01-06 16:30:01 +0000

    使用 google 代理 - 已成功

--- app/lib/modules/llm/providers/google.ts
+++ app/lib/modules/llm/providers/google.ts
@@ -43,7 +43,7 @@ export default class GoogleProvider extends BaseProvider {
     }
 
     const google = createGoogleGenerativeAI({
-      baseURL: 'https://gemini-api.deno.dev/v1beta',
+      baseURL: 'https://gemini-openaiapi-gg.deno.dev/v1beta',
       apiKey,
     });
 
